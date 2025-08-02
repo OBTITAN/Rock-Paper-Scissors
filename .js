286a13a2c443e6e84fe244f3cb7dd94a3b64a846
Emmanuel Ninsin <emmanuel7ninsin@gmail.com>
2025-08-02 23:39:40 +0000

    Applied game interactivity

--- .js
+++ .js
@@ -24,5 +24,49 @@ const getHumanChoice = () => {
 
 
 
-let humnanScore = 0;
+
+
+const playGame = () => {
+
+let humanScore = 0;
 let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    if(humanChoice == 'rock' && computerChoice == 'scissors' ){
+        console.log(`Human Wins, ${humanChoice} beats ${computerChoice}`)
+        humanChoice += 1
+    }
+    else if(humanChoice == 'scissors' && computerChoice == 'paper'){
+         console.log(`Human Wins, ${humanChoice} beats ${computerChoice}`)
+         humanChoice += 1
+    }
+    else if(humanChoice == 'paper' && computerChoice == 'rock'){
+         console.log(`Human Wins, ${humanChoice} beats ${computerChoice}`)
+         humanChoice += 1
+    }
+    else if(humanChoice == 'scissors' && computerChoice == 'rock'){
+        console.log(`Computer Wins, ${computerChoice} beats ${humanChoice}`)
+        computerChoice += 1
+    }
+    else if(humanChoice == 'paper' && computerChoice == 'scissors'){
+        console.log(`Computer Wins, ${computerChoice} beats ${humanChoice}`)
+        computerChoice += 1
+    }
+    else if(humanChoice == 'rock' && computerChoice == 'paper'){
+        console.log(`Computer Wins, ${computerChoice} beats ${humanChoice}`)
+        computerChoice += 1
+    }
+    else{
+        console.log(`It's a draw`)
+        computerChoice += 0
+        humanChoice += 0
+    }
+}
+
+for(let round = 1; round < 6; round++){
+playRound(getHumanChoice(), getComputerChoice())
+}
+
+}
+
+playGame();
